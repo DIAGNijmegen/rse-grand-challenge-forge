235ce261e6dc89b7b400f491403140e4222dce06
Chris van Run <Chris.vanrun@radboudumc.nl>
2023-12-05 12:55:18 +0100

    Add example upload script

--- grand_challenge_forge/forge.py
+++ grand_challenge_forge/forge.py
@@ -1,6 +1,8 @@
+import json
 import logging
 import os
 import shutil
+from copy import deepcopy
 from pathlib import Path
 
 from cookiecutter.generate import generate_files
@@ -8,6 +10,7 @@ from cookiecutter.generate import generate_files
 from grand_challenge_forge.exceptions import OutputOverwriteError
 from grand_challenge_forge.schemas import validate_pack_context
 from grand_challenge_forge.utils import cookiecutter_context as cc
+from grand_challenge_forge.utils import extract_slug
 
 logger = logging.getLogger(__name__)
 
@@ -26,6 +29,13 @@ def generate_challenge_pack(*, context, output_directory, force=False):
     _handle_existing(pack_dir, force=force)
 
     generate_readme(context, output_directory)
+
+    for phase in context["challenge"]["phases"]:
+        phase_dir = pack_dir / phase["slug"]
+        generate_upload_to_archive_script(
+            context={"phase": phase},
+            output_directory=phase_dir,
+        )
 
     return pack_dir
 
@@ -48,3 +58,50 @@ def generate_readme(context, output_directory):
         skip_if_file_exists=False,
         output_dir=output_directory,
     )
+
+
+def generate_upload_to_archive_script(context, output_directory):
+    context = deepcopy(context)
+
+    # Cannot use filters in directory names so generate it here
+    archive_slug = extract_slug(context["phase"]["archive"]["url"])
+    context["phase"]["archive"]["slug"] = archive_slug
+
+    expected_cases, create_files_func = _gen_expected_cases(
+        inputs=context["phase"]["inputs"],
+        output_directory=output_directory
+        / f"upload-to-archive-{archive_slug}",
+    )
+    context["phase"]["expected_cases"] = expected_cases
+
+    generate_files(
+        repo_dir=PARTIALS_PATH / "upload-to-archive-script",
+        context=cc(context),
+        overwrite_if_exists=False,
+        skip_if_file_exists=False,
+        output_dir=output_directory,
+    )
+
+    create_files_func()
+
+
+def _gen_expected_cases(inputs, output_directory, n=3):
+    to_create_files = []
+    result = []
+    for i in range(0, n):
+        item_files = []
+        for j in range(0, len(inputs)):
+            item_files.append(
+                f"case{i}_file{j}.example"
+                if len(inputs) > 1
+                else f"file{i}.example"
+            )
+        to_create_files.extend(item_files)
+        result.append(item_files)
+
+    def create_files():
+        for filename in to_create_files:
+            with open(output_directory / filename, "w") as f:
+                f.write("<placeholder data as an example>")
+
+    return [json.dumps(entry) for entry in result], create_files
--- grand_challenge_forge/utils.py
+++ grand_challenge_forge/utils.py
@@ -1,3 +1,6 @@
+import re
+
+
 def truncate_with_epsilons(value, max_length=32, epsilon="..."):
     if len(str(value)) > max_length:
         truncated_value = str(value)[: max_length - len(epsilon)] + epsilon
@@ -15,3 +18,19 @@ def cookiecutter_context(context):
             ],
         }
     }
+
+
+def extract_slug(url):
+    # Define a regex pattern to match the slug in the URL
+    pattern = r"/([^/]+)/*$"
+
+    # Use re.search to find the match
+    match = re.search(pattern, url)
+
+    # If a match is found, extract and return the slug
+    if match:
+        slug = match.group(1)
+        return slug
+    else:
+        # Return None or handle the case where no match is found
+        return None
