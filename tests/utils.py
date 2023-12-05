aa2bb5a90d1412d48cb9909b138fd640bd9e5c57
Chris van Run <Chris.vanrun@radboudumc.nl>
2023-12-05 17:29:24 +0100

    Remove unused test util function

--- tests/utils.py
+++ tests/utils.py
@@ -62,33 +62,3 @@ def pack_context_factory(**kwargs):
     for k, v in kwargs.items():
         result["challenge"][k] = v
     return result
-
-
-def deep_merge_dicts(dict1, dict2):
-    """Deep merge two dictionaries, including merging lists of dictionaries."""
-    if not isinstance(dict1, dict) or not isinstance(dict2, dict):
-        return dict2
-
-    result = deepcopy(dict1)  # Create a copy of dict1
-
-    for key, value2 in dict2.items():
-        if key in result:
-            value1 = result[key]
-            if isinstance(value1, list) and isinstance(value2, list):
-                # If both values are lists, merge them
-                result[key] = [
-                    deep_merge_dicts(v1, v2)
-                    for v1, v2 in zip(value1, value2, strict=False)
-                ]
-                result[key] = result[key] + value2[len(value1) :]
-            elif isinstance(value1, dict) and isinstance(value2, dict):
-                # If both values are dictionaries, recursively merge them
-                result[key] = deep_merge_dicts(value1, value2)
-            else:
-                # Otherwise, override the value in dict1 with the value in dict2
-                result[key] = value2
-        else:
-            # If the key is not in dict1, add it to the result
-            result[key] = value2
-
-    return result
