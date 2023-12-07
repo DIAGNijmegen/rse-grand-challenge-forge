984ecd3371e4048f7480edbe6f9a2cc4c1049654
Chris van Run <Chris.vanrun@radboudumc.nl>
2023-12-07 10:18:44 +0100

    Test for phase dirs in packs

--- tests/test_pack_generation.py
+++ tests/test_pack_generation.py
@@ -2,10 +2,14 @@ from grand_challenge_forge.forge import generate_challenge_pack
 from tests.utils import pack_context_factory
 
 
-def test_pack_readme(tmp_path):
+def test_for_pack_content(tmp_path):
+    context = pack_context_factory()
     pack_dir = generate_challenge_pack(
-        context=pack_context_factory(),
+        context=context,
         output_directory=tmp_path,
     )
 
     assert (pack_dir / "README.md").exists
+
+    for phase in context["challenge"]["phases"]:
+        assert (pack_dir / phase["slug"]).exists
