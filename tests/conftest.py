2aa8c6b5109250f9f3a29f1448928b9345df2279
Chris van Run <chris.vanrun@radboudumc.nl>
2025-04-08 14:59:52 +0200

    Per test unique id (instead of per xdist process)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,9 @@
+import uuid
 from pathlib import Path
 
 import pytest
 
 
 @pytest.fixture
-def testrun_zpath(testrun_uid):
-    return Path(testrun_uid)
+def testrun_zpath():
+    return Path(str(uuid.uuid4()))
